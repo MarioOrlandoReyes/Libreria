3b429c7ab29610480206d40d707613153ca56840
Mario Reyes <mario.reyes@sferea.com>
2022-05-17 21:01:19 -0500

    Primer Commit

---
+++ Sources/Libreria/Libreria.swift
@@ -0,0 +1,10 @@
+public struct Libreria {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func sayHello() -> String{
+        "Hello Mario"
+    }
+}
